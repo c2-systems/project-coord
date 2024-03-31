eaead2dceeb6c0fb91b160c3cf883f39ac377c7f
jsteffensen <jsteffensen@gmail.com>
2024-03-31 14:35:30 +0200

    dynamic load DOM elements

--- SiteAssets/js/init.js
+++ SiteAssets/js/init.js
@@ -13,6 +13,9 @@
 		console.log(event);
 	});
 	
+	addSelectItems();
+	addSnippet();
+	addMessages();
 
   }); // end of document ready
   
@@ -40,3 +43,93 @@ let progressState;
 	$('.progress').hide();  
 	$('.progress-spacer').show();  
   }
+  
+	function getSelectItemHTML() {
+
+		let text = '$Allan';
+		objDiv = $('<div></div>', {'class': 'select-item'});
+		objSpan = $('<span></span>');
+		objSpan.text(text);
+		objBadge = $('<span>4 asd</span>');
+		objBadge.addClass('new');
+		objBadge.addClass('badge');
+		
+		objDiv.append(objSpan);
+		objDiv.append(objBadge);
+
+		objDiv.click((event)=>{
+			$('.selected-item').removeClass('selected-item');
+			$(event.target).addClass('selected-item');
+			console.log(event);
+		});
+
+		return objDiv;
+	}  
+
+	function getSnippetHTML(message) {
+
+		objDiv = $('<div></div>', {'class': 'document-snippet'});
+		objSpanText = $('<span></span>');
+		objPara = $('<p></p>');
+		objPara.text(message);
+		objBadge = $('<span>erwtertertertert sdf</span>');
+		objBadge.addClass('badge');
+		objBadge.addClass('black-text');
+		
+		objSpanText.append(objPara);
+		objDiv.append(objSpanText);
+		objDiv.append(objBadge);
+
+		return objDiv;
+	}
+	
+	function getMessageHTML(message) {
+
+		objDiv = $('<div></div>', {'class': 'message-item'});
+		objSpanText = $('<span></span>');
+		objPara = $('<p></p>');
+		objPara.text(message);
+		
+		objBadge1 = $('<span>23</span>');
+		objBadge1.addClass('new');
+		objBadge1.addClass('badge');
+		objBadge1.addClass('deep-orange');
+		objBadge1.addClass('lighten-1');
+		
+		objBadge2 = $('<span>67</span>');
+		objBadge2.addClass('new');
+		objBadge2.addClass('badge');
+		
+		objSpanText.append(objPara);
+		objDiv.append(objSpanText);
+		objDiv.append(objBadge1);
+		objDiv.append(objBadge2);
+
+		return objDiv;
+	}	
+
+function addSelectItems() {
+
+	for(let i=0; i<6; i++) {
+		let selectHtml = getSelectItemHTML();
+		$('.left-col').append(selectHtml);
+	}
+}
+
+function addSnippet() {
+	
+	let msg = '$Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+	let msgHtml = getSnippetHTML(msg);
+	$('.right-col').append(msgHtml);
+}
+
+function addMessages() {
+	
+	let msg = '$Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+	for(let i=0; i<6; i++) {
+		let msgHtml = getMessageHTML(msg);
+		$('.right-col').append(msgHtml);
+	}
+}
